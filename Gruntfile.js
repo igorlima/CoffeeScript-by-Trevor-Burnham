f55a71172e17c16f82eb595d31811bf732254c70
Igor Lima <YC26IR1@macys.com>
2013-07-22 17:08:11 -0300

    Use js files from lib foulder

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jasmine: {
       main: {
-        src: ['src/js/**/*.js'],
+        src: ['lib/**/*.js', 'src/js/**/*.js'],
         options: {
           specs:   'spec/js/**/*.js',
           version: '1.3.1'
