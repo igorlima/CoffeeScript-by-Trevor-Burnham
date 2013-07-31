a3fcaaedc839ad8ff93b8ef5ae0966390f690c97
Igor Lima <YC26IR1@macys.com>
2013-07-31 16:23:53 -0300

    Caught untreated exceptions

--- spec/lib/sauce-conf.js
+++ spec/lib/sauce-conf.js
@@ -18,9 +18,18 @@ var webdriver = require('wd'),
     };
 
 browser.on("status", function(info) {
-  console.log("\x1b[36m%s\x1b[0m", info);
+  //console.log("\x1b[36m%s\x1b[0m", info);
 });
 
 browser.on("command", function(meth, path, data) {
-  console.log(" > \x1b[33m%s\x1b[0m: %s", meth, path, data || "");
+  //console.log(" > \x1b[33m%s\x1b[0m: %s", meth, path, data || "");
+});
+
+/**
+Vows Errored » callback not fired
+http://birkett.no/blog/2013/05/01/vows-errored-callback-not-fired/
+*/
+process.on('uncaughtException', function(err) {
+  browser.quit();
+  console.log('Caught exception: ' + err );
 });
