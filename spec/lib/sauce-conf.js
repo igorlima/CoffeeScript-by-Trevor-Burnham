a4522f978e3d093e914c352c4d93b7de4dd478ba
Igor Lima <igor.lima@evolut.io>
2013-08-04 23:43:57 -0300

    Refactor

--- spec/lib/sauce-conf.js
+++ spec/lib/sauce-conf.js
@@ -5,6 +5,7 @@ var webdriver = require('wd'),
     username = process.env.SAUCE_USERNAME,
     accessKey = process.env.SAUCE_ACCESS_KEY,
     browser = exports.browser = webdriver.remote(host, port, username, accessKey),
+    promiseBrowser = exports.promiseBrowser = webdriver.promiseRemote(host, port, username, accessKey),
     desired = exports.desired = {
       "browserName": "chrome",
       "version"    : "",
@@ -15,15 +16,20 @@ var webdriver = require('wd'),
       "build"      : process.env.TRAVIS_BUILD_NUMBER ? process.env.TRAVIS_BUILD_NUMBER : "dev-tests",
       "tunnel-identifier": process.env.TRAVIS_JOB_NUMBER ? process.env.TRAVIS_JOB_NUMBER : "Scrabble",
       "record-video": true
+    },
+
+    info = function(info) {
+      //console.log("\x1b[36m%s\x1b[0m", info);
+    },
+
+    command = function(meth, path, data) {
+      //console.log(" > \x1b[33m%s\x1b[0m: %s", meth, path, data || "");
     };
 
-browser.on("status", function(info) {
-  //console.log("\x1b[36m%s\x1b[0m", info);
-});
-
-browser.on("command", function(meth, path, data) {
-  //console.log(" > \x1b[33m%s\x1b[0m: %s", meth, path, data || "");
-});
+browser.on("status", info);
+browser.on("command", command);
+promiseBrowser.on("status", info);
+promiseBrowser.on("command", command);
 
 /**
 Vows Errored » callback not fired
--- spec/lib/saucelauncher-webdriver.js
+++ spec/lib/saucelauncher-webdriver.js
@@ -6,11 +6,38 @@ var config  = require('./sauce-conf.js'),
     Q       = require("q"),
     request = require("request"),
     $       = require('jquery'),
-    async   = require('async'),
     username = process.env.SAUCE_USERNAME,
     accessKey = process.env.SAUCE_ACCESS_KEY,
-    browser = config.browser,
-    desired = {};
+    browser = config.promiseBrowser,
+    desired = {},
+
+    // general rest call helper function using promises
+    api = function (url, method, data) {
+      var deferred = Q.defer();
+      request({
+        method: method,
+        uri: ["https://", username, ":", accessKey, "@saucelabs.com/rest", url].join(""),
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+      }, function (error, response, body) {
+        deferred.resolve(response.body);
+      });
+      return deferred.promise;
+    },
+
+    waitUntilResultsAreAvailable = function(js, timeout, start) {
+      var now = new Date();
+      start = start || now;
+
+      if (now - start > timeout)
+        throw new Error("Timeout: Element not there");
+
+      return browser.eval(js)
+        .then(function (jsValue) {
+          if (jsValue !== null) { return jsValue; }
+          else { return waitUntilResultsAreAvailable(js, timeout, start); }
+        });
+    };
 
 $.extend(desired, config.desired, {
   "browserName": process.argv[2],
@@ -18,75 +45,20 @@ $.extend(desired, config.desired, {
   "platform"   : process.argv[4]
 });
 
-// general rest call helper function using promises
-var api = function (url, method, data) {
-  var deferred = Q.defer();
-  request({
-    method: method,
-    uri: ["https://", username, ":", accessKey, "@saucelabs.com/rest", url].join(""),
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
-  }, function (error, response, body) {
-    deferred.resolve(response.body);
-  });
-  return deferred.promise;
-};
-
-function waitUntilResultsAreAvailable(js, timeout, start, callback) {
-  var now = new Date();
-  start = start || now;
-
-  if (now - start > timeout) {
-    callback( new Error("Timeout: Element not there") );
-  } else {
-    browser.eval(js, function(err, jsValue) {
-      jsValue !== null ? callback(null, jsValue) : waitUntilResultsAreAvailable(js, timeout, start, callback);
-    });
-  }
-}
-
-async.waterfall([
-
-  function(callback) {
-    browser.init(desired, function(){
-      callback(null);
-    });
-  },
-
-  function(callback) {
-    browser.get("http://localhost:8080", function(){
-      callback(null);
-    });
-  },
-
-  function(callback) {
-    var script = "jasmine.currentEnv_.currentRunner_.results().failedCount";
-    waitUntilResultsAreAvailable(script, 15000, null, callback);
-  },
-
-  function(failedCount, callback) {
-    var data = {
-      "passed": failedCount === 0
-    };
-
-    api(["/v1/", username, "/jobs/", browser.sessionID].join(""), "PUT", data)
-      .then(function(body) {
-        callback(null, body);
-      });
-  },
-
-  function(body, callback) {
-    console.warn("Check out test results at http://saucelabs.com/jobs/" + browser.sessionID + "\n");
-    //console.log(body);
-    callback(null);
-  },
-
-  function(callback) {
-    browser.quit(function(err){
-      callback(err);
-    });
-  }
-
-], function(err) {
-  err && console.error('Caught exception: ' + err.stack);
-});
+// test case
+browser.init(desired).then(function () {
+  return browser.get("http://localhost:8080");
+}).then(function () {
+  var script = "jasmine.currentEnv_.currentRunner_.results().failedCount";
+  return waitUntilResultsAreAvailable(script, 5000);
+}).then(function (failedCount) {
+  var data = {
+    "passed": failedCount === 0
+  };
+  return api(["/v1/", username, "/jobs/", browser.sessionID].join(""), "PUT", data);
+}).then(function (body) {
+  console.warn("Check out test results at http://saucelabs.com/jobs/" + browser.sessionID + "\n");
+  //console.log(body);
+}).fin(function () {
+  return browser.quit();
+}).done();
