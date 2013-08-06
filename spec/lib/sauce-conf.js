bc15d9dfc35771c9ea50b76045a30ee55ba007c9
Igor Lima <YC26IR1@macys.com>
2013-08-06 11:59:44 -0300

    Refactor

--- spec/lib/sauce-conf.js
+++ spec/lib/sauce-conf.js
@@ -1,76 +1,32 @@
 var webdriver = require('wd'),
-    Q         = require("q"),
-    request   = require("request"),
     async     = require('async'),
     argv      = require('optimist').argv,
     exports   = module.exports = {},
+
+    libraryName = exports.libraryName = "Scrabble",
+
+    auth      = exports.browser = {
+      username:  process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+      build:     process.env.TRAVIS_BUILD_NUMBER || "dev-tests",
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER || libraryName
+    },
+
     host      = "ondemand.saucelabs.com",
     port      = 80,
-    username  = process.env.SAUCE_USERNAME,
-    accessKey = process.env.SAUCE_ACCESS_KEY,
-    browser = exports.browser = webdriver.remote(host, port, username, accessKey),
+    browser = exports.browser = webdriver.remote(host, port, auth.username, auth.accessKey),
 
     desired = exports.desired = {
       "browserName": argv.browserNameSL || "chrome",
       "version"    : argv.versionSL     || "",
       "platform"   : argv.platformSL    || "Linux",
-      "tags"       : ["Scrabble", "test"],
-      "name"       : "Scrabble tests",
+      "tags"       : [libraryName, "test"],
+      "name"       : libraryName + " tests",
       "public"     : "public",
-      "build"      : process.env.TRAVIS_BUILD_NUMBER || "dev-tests",
-      "tunnel-identifier": process.env.TRAVIS_JOB_NUMBER || "Scrabble",
+      "build"      : auth.build,
+      "tunnel-identifier": auth.tunnelIdentifier,
       "record-video": true
-    },
-
-    api = function (url, method, data) {
-      var deferred = Q.defer();
-      request({
-        method: method,
-        uri: ["https://", username, ":", accessKey, "@saucelabs.com/rest", url].join(""),
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data)
-      }, function (error, response, body) {
-        deferred.resolve(response.body);
-      });
-      return deferred.promise;
-    },
-
-    waitUntilResultsAreAvailable = function(js, timeout, start, callback) {
-      var now = new Date();
-      start = start || now;
-
-      if (now - start > timeout) {
-        callback( new Error("Timeout: Element not there") );
-      } else {
-        browser.eval(js, function(err, jsValue) {
-          if (jsValue !== null) callback(null, {resultScript: jsValue});
-          else waitUntilResultsAreAvailable(js, timeout, start, callback);
-        });
-      }
     };
-
-exports.updateJobStatus = function(script, callback) {
-  async.waterfall([
-
-    function(callback) {
-      waitUntilResultsAreAvailable(script, 15000, null, callback);
-    },
-
-    function(obj, callback) {
-      var data = resultScript = obj.resultScript,
-          url  = ["/v1/", username, "/jobs/", browser.sessionID].join("");
-      data.passed = resultScript.passed || resultScript.failedCount === 0;
-
-      api(url, "PUT", data).then( function(body) {
-        obj.body = body;
-        console.warn("Check out test results at http://saucelabs.com/jobs/" + browser.sessionID + "\n");
-        callback(null, obj);
-      });
-    }
-  ], function(err, result) {
-    callback(err, result);
-  });
-};
 
 browser.on("status", function(info) {
   //console.log("\x1b[36m%s\x1b[0m", info);
--- spec/lib/saucelauncher-webdriver.js
+++ spec/lib/saucelauncher-webdriver.js
@@ -1,36 +1,6 @@
-/**
- * JavaScript tests integration with Sauce
- * https://saucelabs.com/docs/javascript-unit-tests-integration
- */
-var config  = require('./sauce-conf.js'),
-    async   = require('async'),
-    browser = config.browser,
-    desired = config.desired;
+var integrationTest = require('./sauce-javascript-tests-integration.js');
 
-async.waterfall([
-
-  function(callback) {
-    browser.init(desired, function(){
-      callback(null);
-    });
-  },
-
-  function(callback) {
-    browser.get("http://localhost:8080", function(){
-      callback(null);
-    });
-  },
-
-  function(callback) {
-    config.updateJobStatus("{failedCount: jasmine.currentEnv_.currentRunner_.results().failedCount}", callback);
-  },
-
-  function(result, callback) {
-    browser.quit(function(err){
-      callback(err);
-    });
-  }
-
-], function(err) {
-  err && console.error('Caught exception: ' + err.stack);
-});
+integrationTest(
+  "http://localhost:8080",
+  "{failedCount: jasmine.currentEnv_.currentRunner_.results().failedCount}"
+);
